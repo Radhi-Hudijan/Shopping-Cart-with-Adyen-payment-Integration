27605332642c3eea6b19f73cdd292559050eb266
Radhi Hudijan <radhif2009@gmail.com>
2022-08-22 22:29:52 +0200

    server updated with axios instead of Adyen lib

--- server/server.js
+++ server/server.js
@@ -1,24 +1,16 @@
+const { default: axios } = require("axios");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const { v4: uuidv4 } = require("uuid");
-const { Client, CheckoutAPI, hmacValidator } = require("@adyen/api-library");
 
 //init app
-app = express();
+const app = express();
 
 // parse json body
 app.use(express.json());
 
 //parse url-encoded bodies
 app.use(express.urlencoded({ extended: true }));
-
-//Adyen node js configuration
-const client = new Client({
-  apiKey: process.env.REACT_APP_ADYEN_API_KEY,
-  environment: "TEST",
-});
-const checkout = new CheckoutAPI(client);
-const validator = new hmacValidator();
 
 /* ################# API ENDPOINTS ###################### */
 app.post("/api/sessions", async (req, res) => {
@@ -33,15 +25,24 @@ app.post("/api/sessions", async (req, res) => {
     // is https or http
     const protocol = req.socket.encrypted ? "https" : "http";
 
-    const response = await checkout.sessions({
-      amount: { currency: "EUR", value: paidAmount }, // value is 10€ in minor units
-      countryCode: "NL",
-      merchantAccount: process.env.REACT_APP_ADYEN_MERCHANT_ACCOUNT, // required
-      reference: "Radhi_checkoutChallenge",
-      returnUrl: `${protocol}://localhost:3000/redirect?orderRef=${orderRef}`, // to handle the redirect
-    });
-
-    res.json(response);
+    const response = await axios.post(
+      "https://checkout-test.adyen.com/v69/sessions",
+      {
+        amount: { currency: "EUR", value: paidAmount }, // value is 10€ in minor units
+        countryCode: "NL",
+        merchantAccount: process.env.REACT_APP_ADYEN_MERCHANT_ACCOUNT, // required
+        reference: "Radhi_checkoutChallenge",
+        returnUrl: `${protocol}://localhost:3000/redirect?orderRef=${orderRef}`, // to handle the redirect
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-API-Key": process.env.REACT_APP_ADYEN_API_KEY,
+        },
+      }
+    );
+    console.log(response.data);
+    res.json(response.data);
   } catch (error) {
     console.error(`Error is:${error.message}, error code: ${error.errorCode}`);
   }
@@ -50,37 +51,37 @@ app.post("/api/sessions", async (req, res) => {
 // I left the webhook notification  out of scope because the HMAC key is not provided
 
 // Receive webhook notifications
-app.post("/notification", async (req, res) => {
-  try {
-    // get the notification request from POST body
-    const notificationRequestItems = req.body.notificationItems;
+// app.post("/notification", async (req, res) => {
+//   try {
+// get the notification request from POST body
+//     const notificationRequestItems = req.body.notificationItems;
 
-    notificationRequestItems.forEach((item) => {
-      if (
-        validator.validateHMAC(
-          item.NotificationRequestItem,
-          process.env.REACT_APP_ADYEN_HMAC_KEY
-        )
-      ) {
-        const eventCode = item.NotificationRequestItem.eventCode;
+//     notificationRequestItems.forEach((item) => {
+//       if (
+//         validator.validateHMAC(
+//           item.NotificationRequestItem,
+//           process.env.REACT_APP_ADYEN_HMAC_KEY
+//         )
+//       ) {
+//         const eventCode = item.NotificationRequestItem.eventCode;
 
-        if (eventCode === "AUTHORISATION") {
-          if (item.NotificationRequestItem.success === "true") {
-            //I left this part out of scope because
-            //payment was successful
-          } else {
-            //payment was refused
-          }
-        } else {
-          console.log("Non-valid Notification");
-        }
-      }
-    });
-    res.status(200).send("[accepted]");
-  } catch (error) {
-    console.log(error);
-  }
-});
+//         if (eventCode === "AUTHORISATION") {
+//           if (item.NotificationRequestItem.success === "true") {
+//I left this part out of scope because
+//payment was successful
+//           } else {
+//payment was refused
+//           }
+//         } else {
+//           console.log("Non-valid Notification");
+//         }
+//       }
+//     });
+//     res.status(200).send("[accepted]");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 /* ################# END OF API ENDPOINTS ####################### */
 
